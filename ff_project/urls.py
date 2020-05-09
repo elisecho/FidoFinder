5021a95db48136da3358bdf7bbd6b40a668d8d8a
sdalnodar <sdalnodar@gmail.com>
2020-05-09 13:25:37 -0400

    working on maps

--- ff_project/urls.py
+++ ff_project/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
     path('', include('ff_app.urls')),
+    path('maps', include('maps.urls'))
 ]
